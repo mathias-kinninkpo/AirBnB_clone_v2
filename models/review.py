5ace00ee0742726e1740365bb4ee2d804a81500e
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-23 11:34:29 +0100

    model review

--- models/review.py
+++ models/review.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""This is the review class"""
-from sqlalchemy.ext.declarative import declarative_base
+""" Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, String, ForeignKey
+from models.user import User
+from models.place import Place
 
 
 class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
+    """ Review classto store review information """
     __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey(Place.id), nullable=False)
+    user_id = Column(String(60), ForeignKey(User.id), nullable=False)
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
