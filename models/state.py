781a0f7db596206986128c16d118b4b0a0633e98
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-23 11:34:48 +0100

    model state

--- models/state.py
+++ models/state.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+""" State Module for HBNB project """
+import os
 import models
-from models.city import City
-import shlex
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    cities = relationship("City", cascade="all, delete", backref="state")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    if not os.getenv("HBNB_TYPE_STORAGE") == "db":
+        @property
+        def cities(self):
+            """list of cities"""
+            all_cities = list(models.storage.all(models.City).values())
+            return list(filter((lambda c: c.state_id == self.id), all_cities))
