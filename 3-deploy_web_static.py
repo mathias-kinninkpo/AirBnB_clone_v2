b58956a25a09cc8c5ce1018a2f2c6de4d215b53a
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-03-10 20:05:14 +0100

    3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -48,26 +48,26 @@ def do_deploy(archive_path):
     if put(archive_path, "/tmp/{}".format(file)).failed:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed:
+        format(name)).failed:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed:
+        format(name)).failed:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed:
+        format(file, name)).failed:
         return False
     if run("rm /tmp/{}".format(file)).failed:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed:
+        "/data/web_static/releases/{}/".format(name, name)).failed:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed:
+        format(name)).failed:
         return False
     if run("rm -rf /data/web_static/current").failed:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed:
+        format(name)).failed:
         return False
     return True
 
